c8ff0287caebaf936f70c208d1cfea4444f5b2e3
Marcos Corsi <marcosvcorsi@gmail.com>
2023-02-27 17:24:58 -0300

    feat: add update survivor providers to survivor module

--- src/survivors/survivors.module.ts
+++ src/survivors/survivors.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SurvivorsController } from './survivors.controller';
+import { InMemorySurvivorsRepository } from './survivors.repository';
 import { SurvivorsService } from './survivors.service';
 
 @Module({
   controllers: [SurvivorsController],
-  providers: [SurvivorsService],
+  providers: [
+    SurvivorsService,
+    {
+      provide: 'ISurvivorsRepository',
+      useClass: InMemorySurvivorsRepository,
+    },
+  ],
 })
 export class SurvivorsModule {}
