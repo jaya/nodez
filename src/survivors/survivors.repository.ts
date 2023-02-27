fd52ca27d620dfd9627a61359994deecd8d84b91
Marcos Corsi <marcosvcorsi@gmail.com>
2023-02-27 17:23:18 -0300

    feat: add survivors repository

---
+++ src/survivors/survivors.repository.ts
@@ -0,0 +1,49 @@
+import { Gender, Survivor } from './survivor.entity';
+
+// Here i'm using an abstract class to be able to inject after
+export interface ISurvivorsRepository {
+  findById(id: string): Promise<Survivor | null>;
+  update(id: string, data: Partial<Survivor>): Promise<Survivor>;
+}
+
+// TO-DO Unit tests if we are going to keep it, or remove do add tests with integrated database
+export class InMemorySurvivorsRepository implements ISurvivorsRepository {
+  private survivors: Survivor[];
+
+  constructor() {
+    this.survivors = [
+      // TO-DO this is a mock for testing, remove when the feature to create exists
+      {
+        id: 'any_id',
+        age: 18,
+        name: 'John Doe',
+        gender: Gender.MALE,
+        latitude: 1,
+        longitude: 1,
+      },
+    ];
+  }
+
+  async findById(id: string): Promise<Survivor> {
+    return this.survivors.find((survivor) => survivor.id === id);
+  }
+
+  async update(id: string, data: Partial<Survivor>): Promise<Survivor> {
+    const survivorIndex = this.survivors.findIndex(
+      (survivor) => survivor.id === id,
+    );
+
+    if (survivorIndex === -1) {
+      throw new Error('Survivor not found in memory list');
+    }
+
+    const existingSurvivor = this.survivors[survivorIndex];
+
+    this.survivors[survivorIndex] = {
+      ...existingSurvivor,
+      ...data,
+    };
+
+    return this.survivors[survivorIndex];
+  }
+}
