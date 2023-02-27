163e9cbd9a70ffae28c63280d45b3dc983a2f7e3
Marcos Corsi <marcosvcorsi@gmail.com>
2023-02-27 17:23:36 -0300

    feat: add update survivor to service

--- src/survivors/survivors.service.spec.ts
+++ src/survivors/survivors.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Gender } from './survivor.entity';
+import {
+  InMemorySurvivorsRepository,
+  ISurvivorsRepository,
+} from './survivors.repository';
 import { SurvivorsService } from './survivors.service';
 
 describe('SurvivorsService', () => {
   let service: SurvivorsService;
+  let repository: ISurvivorsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SurvivorsService],
+      providers: [
+        SurvivorsService,
+        {
+          provide: 'ISurvivorsRepository',
+          useClass: InMemorySurvivorsRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<SurvivorsService>(SurvivorsService);
+    repository = module.get<ISurvivorsRepository>('ISurvivorsRepository');
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return an Error if survivor does not exists', async () => {
+    await expect(
+      service.update({ id: 'any_id', latitude: 1, longitude: 1 }),
+    ).rejects.toThrow();
+  });
+
+  it('should be able to update a survivor and return', async () => {
+    const survivor = {
+      id: 'any_id',
+      name: 'any_name',
+      age: 18,
+      gender: Gender.MALE,
+      latitude: 1,
+      longitude: 1,
+    };
+
+    const params = {
+      id: 'any_id',
+      latitude: 2,
+      longitude: 2,
+    };
+
+    jest.spyOn(repository, 'findById').mockResolvedValue(survivor);
+    jest.spyOn(repository, 'update').mockResolvedValue({
+      ...survivor,
+      ...params,
+    });
+
+    const response = await service.update(params);
+
+    expect(response).toMatchObject(params);
   });
 });
--- src/survivors/survivors.service.ts
+++ src/survivors/survivors.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Survivor } from './survivor.entity';
+import { ISurvivorsRepository } from './survivors.repository';
+
+export type UpdateSurvivorParams = {
+  id: string;
+  latitude: number;
+  longitude: number;
+};
 
 @Injectable()
-export class SurvivorsService {}
+export class SurvivorsService {
+  constructor(
+    @Inject('ISurvivorsRepository')
+    private readonly survivorsRepository: ISurvivorsRepository,
+  ) {}
+
+  async update({
+    id,
+    latitude,
+    longitude,
+  }: UpdateSurvivorParams): Promise<Survivor> {
+    let survivor = await this.survivorsRepository.findById(id);
+
+    if (!survivor) {
+      throw new NotFoundException('Survivor not found');
+    }
+
+    survivor = await this.survivorsRepository.update(id, {
+      latitude,
+      longitude,
+    });
+
+    return survivor;
+  }
+}
