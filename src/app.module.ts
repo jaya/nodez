64f5154fc93475c1e4dcf7c1c414cb04292554ac
Marcos Corsi <marcosvcorsi@gmail.com>
2023-02-27 17:19:32 -0300

    refactor: remove app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SurvivorsModule } from './survivors/survivors.module';
 
 @Module({
   imports: [SurvivorsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
