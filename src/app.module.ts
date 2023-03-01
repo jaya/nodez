55b0b743fb685b177f302c3703368ac416a1ec32
Luis Nascimento <nascimento.lsfo@gmail.com>
2023-03-01 16:39:12 -0300

    Removing unnecessary survivor import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SurvivorsModule } from './survivors/survivors.module';
 
 @Module({
-  imports: [SurvivorsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
